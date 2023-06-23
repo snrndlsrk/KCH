c1291248c4afaa4917cc58150827fbf1e3693bc7
snrndlsrk <6800kch@naver.com>
2023-06-23 10:38:17 +0900

    2023.06.23

---
+++ study0623/static/js/array7.js
@@ -0,0 +1,9 @@
+
+window.onload=function(){
+    // 화면이 전부 로딩 되면 시작하는 함수
+    var icon = document.getElementsByClassName("strapIcon");
+    icon[0].addEventListener("click", function(){
+        // var list = document.getElementsByClassName("menu_list");
+        // list[0].style.display="block";
+    });
+}
