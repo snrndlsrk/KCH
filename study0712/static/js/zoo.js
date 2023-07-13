1bbdc974f7292885a2e694a7315a4fe86d20caf7
snrndlsrk <6800kch@naver.com>
2023-07-13 13:35:57 +0900

    animal

--- study0712/static/js/zoo.js
+++ study0712/static/js/zoo.js
@@ -1,13 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchButton = document.querySelector('.search_button');
-    const searchBar = document.querySelector('.search_bar');
-    const searchClose = document.querySelector('.search_close');
-
-    searchButton.addEventListener('click', function() {
-        searchBar.classList.toggle('show');
+window.onload=function(){
+    var search_bt = document.getElementsByClassName("search_button");
+    search_bt[0].addEventListener("click",function(){
+        var bar = document.querySelectorAll(".search_bar")[0];
+        bar.style.display="block";
     });
-
-    searchClose.addEventListener('click', function() {
-        searchBar.classList.remove('show');
+    var search_close = document.getElementsByClassName("search_close");
+    search_close[0].addEventListener("click",function(){
+        var bar = document.querySelectorAll(".search_bar")[0];
+        bar.style.display="none";
     });
-});
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var searchButton = document.querySelector('.search_button');
+//     var searchBar = document.querySelector('.search_bar');
+//     var searchClose = document.querySelector('.search_close');
+
+//     searchButton.addEventListener('click', function() {
+//         searchBar.classList.toggle('show');
+//     });
+
+//     searchClose.addEventListener('click', function() {
+//         searchBar.classList.remove('show');
+//     });
+// });
